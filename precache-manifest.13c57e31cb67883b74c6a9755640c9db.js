e1c6bacad64ea92dd605d11f3365f983d386d4ba
Alexey <youth-design@yandex.ru>
2019-02-06 14:08:22 +0300

    Updates

---
+++ precache-manifest.13c57e31cb67883b74c6a9755640c9db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc8e9cfd119a9895ee2d",
+    "url": "/LNU-schedule/static/css/main.a47464ab.chunk.css"
+  },
+  {
+    "revision": "bc8e9cfd119a9895ee2d",
+    "url": "/LNU-schedule/static/js/main.bc8e9cfd.chunk.js"
+  },
+  {
+    "revision": "dd9f1cc1d373a665eafd",
+    "url": "/LNU-schedule/static/js/1.dd9f1cc1.chunk.js"
+  },
+  {
+    "revision": "1c0f228dd8bf3fe88dfe",
+    "url": "/LNU-schedule/static/js/runtime~main.1c0f228d.js"
+  },
+  {
+    "revision": "7126994564708406dc453f20c3fffb26",
+    "url": "/LNU-schedule/static/media/logo.71269945.png"
+  },
+  {
+    "revision": "c4ebf2250af421c9801e2319b521e223",
+    "url": "/LNU-schedule/index.html"
+  }
+];
