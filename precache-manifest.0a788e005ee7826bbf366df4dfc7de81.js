99cf7280f6b1ee2f82bc0c359d17f8551f7a6b37
alexey <youth-design@yandex.ru>
2020-01-19 21:00:32 +0300

    Updates

---
+++ precache-manifest.0a788e005ee7826bbf366df4dfc7de81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7182f91ddd9d7fe0b41",
+    "url": "/LNU-schedule/static/css/main.d92e76b4.chunk.css"
+  },
+  {
+    "revision": "f7182f91ddd9d7fe0b41",
+    "url": "/LNU-schedule/static/js/main.f7182f91.chunk.js"
+  },
+  {
+    "revision": "947c2c02d06fedd1a70c",
+    "url": "/LNU-schedule/static/js/1.947c2c02.chunk.js"
+  },
+  {
+    "revision": "1c0f228dd8bf3fe88dfe",
+    "url": "/LNU-schedule/static/js/runtime~main.1c0f228d.js"
+  },
+  {
+    "revision": "7126994564708406dc453f20c3fffb26",
+    "url": "/LNU-schedule/static/media/logo.71269945.png"
+  },
+  {
+    "revision": "9001f6e15502c8b138ed93b4a9bffa87",
+    "url": "/LNU-schedule/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/LNU-schedule/precache-manifest.63fa3b7ed126a736028d10bb5ac1fbb8.js"
+  "/LNU-schedule/precache-manifest.0a788e005ee7826bbf366df4dfc7de81.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/LNU-schedule/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
